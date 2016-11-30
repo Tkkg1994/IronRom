e93815c30673a4f4e105b62b20006aeda0e7e45b
Luca Grifo <lg@linux.com>
2016-11-30 02:32:36 +0100

    Rebase on latest CPJ6 base - updated adaway - updated supersu - updated xposed - updated magisk - etc etc

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY:8450048:a32211ffb60d4ea8c05e428305c2ea7e935f2c6f; then
-  applypatch EMMC:/dev/block/platform/dw_mmc.0/by-name/BOOT:7966720:4ef736aee05add208075bd82746e6948e0fe1ce1 EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY a32211ffb60d4ea8c05e428305c2ea7e935f2c6f 8450048 4ef736aee05add208075bd82746e6948e0fe1ce1:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY:8450048:397decbf93e7c2d1f45fe95b681a84a8a6aca587; then
+  applypatch EMMC:/dev/block/platform/dw_mmc.0/by-name/BOOT:7966720:e84f31f9fb23815a8455c7d8e43963a63f821e15 EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY 397decbf93e7c2d1f45fe95b681a84a8a6aca587 8450048 e84f31f9fb23815a8455c7d8e43963a63f821e15:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
