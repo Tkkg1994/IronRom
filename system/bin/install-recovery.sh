c58de1d22c42e8318f270a6cfdfcae51fa336a82
Luca Grifo <lg@linux.com>
2017-01-02 01:29:23 +0100

    update most files to CPK2 except apks and jars

--- system/bin/install-recovery.sh
+++ system/bin/install-recovery.sh
@@ -1,6 +1,6 @@
 #!/system/bin/sh
-if ! applypatch -c EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY:8450048:397decbf93e7c2d1f45fe95b681a84a8a6aca587; then
-  applypatch EMMC:/dev/block/platform/dw_mmc.0/by-name/BOOT:7966720:e84f31f9fb23815a8455c7d8e43963a63f821e15 EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY 397decbf93e7c2d1f45fe95b681a84a8a6aca587 8450048 e84f31f9fb23815a8455c7d8e43963a63f821e15:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
+if ! applypatch -c EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY:8507392:c7f6a7f28d053c1c4d374c55552ac57d14301536; then
+  applypatch EMMC:/dev/block/platform/dw_mmc.0/by-name/BOOT:8022016:c86308c775559f674190c1a9c6ca1e3cc44937e8 EMMC:/dev/block/platform/dw_mmc.0/by-name/RECOVERY c7f6a7f28d053c1c4d374c55552ac57d14301536 8507392 c86308c775559f674190c1a9c6ca1e3cc44937e8:/system/recovery-from-boot.p && log -t recovery "Installing new recovery image: succeeded" || log -t recovery "Installing new recovery image: failed"
 else
   log -t recovery "Recovery image already installed"
 fi
