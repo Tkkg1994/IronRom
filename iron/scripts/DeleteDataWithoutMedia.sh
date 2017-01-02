6a313b62dfe0ff41b93b9c7437fc3c5ceed76dc0
Luca Grifo <lg@linux.com>
2017-01-02 02:17:58 +0100

    update all apks and jars (included modded one) update host file, update xposed, update supersu, updated tab s2 apps# Please enter the commit message for your changes. Lines starting

--- iron/scripts/DeleteDataWithoutMedia.sh
+++ iron/scripts/DeleteDataWithoutMedia.sh
@@ -1,2 +1,9 @@
 #!/sbin/sh
-/sbin/busybox find /data -maxdepth 1 -type d ! -path "/data" ! -path "/data/media" | xargs rm -rf
+# Written by Tkkg1994
+
+mount /dev/block/platform/155a0000.ufs/by-name/USERDATA /data
+
+find /data -maxdepth 1 -type d ! -path "/data" ! -path "/data/media" | xargs rm -rf
+
+exit 10
+
